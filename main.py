0f91c3688336294adb3261a5ab6bfbfba6c3a684
nikkie <takuyafjp+develop@gmail.com>
2018-05-04 16:05:04 +0900

    [feat] logging POST JSON
    * 以下を参考にloggingを設定し、POSTされたJSONをログファイルに書き込むようにした
    https://gist.github.com/ibeex/3257877#gistcomment-946336
    http://d.hatena.ne.jp/heavenshell/20111020/1319120610

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 from platform import python_version
 
 from flask import Flask
@@ -7,6 +9,13 @@ from flask import request
 
 
 app = Flask(__name__)
+handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+app.logger.setLevel(logging.INFO) # app.run(debug=False)でinfo以下のログを出すために必要らしい
+app.logger.addHandler(handler)
 
 @app.route('/')
 def hello_world():
@@ -16,7 +25,7 @@ def hello_world():
 @app.route('/response', methods=['POST'])
 def print_data():
     parsed = json.loads(request.data)
-    # TODO: parsedをログに出力
+    app.logger.info(json.dumps(parsed, indent=2))
     response = jsonify({'foo': 'bar'})
     response.status_code = 200
     return response
